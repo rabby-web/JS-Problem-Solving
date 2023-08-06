d36d0701b351dfe8c488a146aa5bddec6f88054c
rabby-web <rabby.webdeveloper@gmail.com>
2023-08-06 22:05:59 +0600

    js p4 add

---
+++ p4.js
@@ -0,0 +1,13 @@
+function minGame(x){
+    if(typeof x !== "number"){
+        return "please give provide a number";
+    }
+    else if(x <= 0){
+        return "please give provide a positive number";
+    }
+    else{
+        const result = (((x * 3) + 10) / 2)-5;
+        return result;
+    }
+}
+console.log(minGame(5))
